ab3a636529cb32b41791ac67f522b7165df01cac
David <dezratty12@gmail.com>
2020-04-17 11:44:39 -0500

    Added manytoOne and onetoMany relationships

--- src/main/java/org/launchcode/crudnotes/models/NoteCategory.java
+++ src/main/java/org/launchcode/crudnotes/models/NoteCategory.java
@@ -1,13 +1,19 @@
 package org.launchcode.crudnotes.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class NoteCategory extends AbstractEntity {
 
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+
+    @OneToMany(mappedBy = "noteCategory")
+    private final List<Note> notes = new ArrayList<>();
 
     public NoteCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
         this.name = name;
@@ -21,6 +27,10 @@ public class NoteCategory extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
     }
 
     @Override
